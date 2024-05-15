1934f1a07ede9119368290582d935be0f592a779
darryl1975 <darryl1975@hotmail.com>
2024-05-15 14:01:53 +0800

    added userskills

---
+++ src/app/user-skills/user-skills.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-skills',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  templateUrl: './user-skills.component.html',
+  styleUrl: './user-skills.component.css'
+})
+export class UserSkillsComponent implements OnInit {
+
+  skillForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.skillForm = this.formBuilder.group({
+      name: '',
+      skills: this.formBuilder.array([])
+    });
+  }
+
+  getSkills() : FormArray {
+
+    return this.skillForm.get("skills") as FormArray;
+  }
+
+  addSkill() {
+    
+  }
+
+  onSubmit() {
+
+  }
+
+}
